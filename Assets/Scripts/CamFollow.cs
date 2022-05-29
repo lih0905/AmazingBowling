035cfb6cb82df3f4d1ba3c328dfef30d13d020c8
Inhwan Lee <lih0905@gmail.com>
2022-05-30 01:10:43 +0900

    Modify camera to follow the ball

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public enum State {
+        Idle, Ready, Tracking
     }
 
-    // Update is called once per frame
-    void Update()
+    private State state {
+        set{
+            switch(value)
+            {
+                case State.Idle:
+                    targetZoomSize = roundReadyZoomSize;
+                    break;
+                case State.Ready:
+                    targetZoomSize = readyShotZoomSize;
+                    break;
+                case State.Tracking:
+                    targetZoomSize = trackingZoomSize;
+                    break;
+            }
+        }
+    }
+
+    public Transform target;
+
+    public float smoothTime = 0.2f;
+
+    private Vector3 lastMovingVelocity;
+    private Vector3 targetPosition;
+
+    private Camera cam;
+    private float targetZoomSize = 5f;
+
+    private const float roundReadyZoomSize = 14.5f;
+    private const float readyShotZoomSize = 5f;
+    private const float trackingZoomSize = 10f;
+
+    private float lastZoomSpeed;
+
+    void Awake()
     {
-        
+        cam = GetComponentInChildren<Camera>();
+        state = State.Idle;
+    }
+
+    private void Move()
+    {
+        targetPosition = target.transform.position;
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, 
+            ref lastMovingVelocity, smoothTime);
+        transform.position = smoothPosition;
+    }
+
+    private void Zoom()
+    {
+        float smoothZoomSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoomSize, 
+            ref lastZoomSpeed, smoothTime);
+        cam.orthographicSize = smoothZoomSize;
+    }
+
+    private void FixedUpdate()
+    {
+        if(target != null)
+        {
+            Move();
+            Zoom();
+        }
+    }
+
+    public void Reset()
+    {
+        state = State.Idle;
+    }
+
+    public void SetTarget(Transform newTarget, State newState)
+    {
+        target = newTarget;
+        state = newState;
     }
 }
